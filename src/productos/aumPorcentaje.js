69db5e953b63971d8082ca5fa9fb0733a85047bc
Pelusa-not <agustinlorenzatto@gmail.com>
2022-07-07 08:33:14 -0300

    Cambie para que cuando es un producto de que tiene dolar se cambie el costodolar

--- src/productos/aumPorcentaje.js
+++ src/productos/aumPorcentaje.js
@@ -1,15 +1,16 @@
 const sweet = require('sweetalert2');
-const select = document.querySelector('#marca')
-const porcentajeInput = document.querySelector('#porcentaje')
-const modificar = document.querySelector('.modificar')
+const select = document.querySelector('#marca');
+const porcentajeInput = document.querySelector('#porcentaje');
+const modificar = document.querySelector('.modificar');
 
 const axios = require("axios");
 require("dotenv").config;
 const URL = process.env.URL;
 
 let marcas;
-
-    const traerMarcas = async()=>{
+let dolar;
+const traerMarcas = async()=>{
+        dolar = parseFloat((await axios.get(`${URL}tipoVenta`)).data.dolar);
         marcas = await axios(`${URL}productos`);
         marcas = marcas.data
         marcas.sort();
@@ -37,10 +38,9 @@ modificar.addEventListener('click',async e=>{
             producto.precio_venta = (producto.precio_venta).toFixed(2)
         }else{
             producto.costodolar = parseFloat((parseFloat(producto.costodolar) + parseFloat(producto.costodolar)*porcentaje/100).toFixed(2));
-            producto.impuestos = (producto.iva === "N") ? (parseFloat(producto.costodolar) * 26 / 100) : (parseFloat(producto.costodolar) * 15 / 100);
-            producto.precio_venta = ((parseFloat(producto.costo) + parseFloat(producto.impuestos))*parseFloat(producto.utilidad)) + (parseFloat(producto.costo) + parseFloat(producto.impuestos))
-            producto.impuestos = (producto.impuestos).toFixed(2)
-            producto.precio_venta = (producto.precio_venta).toFixed(2)
+            producto.impuestos = (producto.iva === "N") ? (parseFloat(producto.costodolar) * 26 / 100).toFixed(2) : (parseFloat(producto.costodolar) * 15 / 100).toFixed(2);
+            producto.precio_venta = ((producto.costodolar + parseFloat(producto.impuestos))*dolar*parseFloat(producto.utilidad)/100) + ((parseFloat(producto.costodolar) + parseFloat(producto.impuestos))*dolar);
+            producto.precio_venta = (producto.precio_venta).toFixed(2);
         }
         await axios.put(`${URL}productos/${producto._id}`,producto)
         sweet.fire({title:`Se Modifico el precio de los productos ${select.value}`})
